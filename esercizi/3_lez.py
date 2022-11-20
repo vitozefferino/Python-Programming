702f6e60efbba5859aa3818343d433c71f6ede07
Vito Zefferino <115221035+vitozefferino@users.noreply.github.com>
2022-11-20 14:28:53 +0000

    Lez.3 max score

--- esercizi/3_lez.py
+++ esercizi/3_lez.py
@@ -1,14 +1,9 @@
-import os
-
 def sum_csv(file_name):
     elements = []    # splitted strings
     sum = 0   # sum value
-
-    if(os.path.getsize(file_name) == 0):
-        return -1
-    else:
-        my_file = open(file_name, 'r')
-        my_file.readline()
+    
+    my_file = open(file_name, 'r')
+    my_file.readline()
     for line in my_file:
         elements = line.split(',')
         value = elements[1]
@@ -18,6 +13,10 @@ def sum_csv(file_name):
             sum = sum + value
         except ValueError:
             pass
-    return sum
+    my_file.close()
+    if(len(elements)==0):
+        return None
+    else:
+        return sum
 
 print(sum_csv('shampoo_sales.csv'))
