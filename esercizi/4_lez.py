95599f1232c79f0681cddc8ab4955498d0d622c0
Vito Zefferino <115221035+vitozefferino@users.noreply.github.com>
2022-11-22 18:01:59 +0000

    Lesson 4

---
+++ esercizi/4_lez.py
@@ -0,0 +1,24 @@
+class CSVFile():
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_data(self):
+        elements = []  # this contains one line at time
+        final = []    # this contains all the lists
+
+        my_file = open(self.name, 'r')
+        my_file.readline()
+        
+        for line in my_file:
+            elements = line.strip('\n').split(',')
+            final.append(elements)
+        my_file.close()
+        
+        if (len(final) == 0):
+            return []
+        else:
+            return final
+
+csv_file = CSVFile('shampoo_sales.csv')
+print(csv_file.get_data())
